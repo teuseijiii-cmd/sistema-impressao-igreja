1d4949475b3b3f98667649a6ac0324a032ea5266
teuseijiii-cmd <teuseijiii@gmail.com>
2026-05-22 20:32:41 -0400

    Create main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,18 @@
+
+ARQUIVO: 6. frontend/src/main.tsx<br/>
+CAMINHO: frontend/src/main.tsx<br/>
+DESCRIÇÃO: Ponto de entrada do React
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
